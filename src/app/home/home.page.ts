ee046d1109cb5a5301e82ae6ef81f8e263d90d24
USRVI-LC12 <USRVI-LC12@duoc.acad>
2025-10-01 21:43:05 -0300

    ADD: Service para llamada a posts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
+import { IonSpinner, IonLabel, IonItem, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { logOutOutline } from 'ionicons/icons';
+import { Post, PostService } from '../services/post-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterModule, IonButtons, IonIcon],
+  imports: [IonSpinner, CommonModule, IonLabel, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterModule, IonButtons, IonIcon, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle],
 })
 export class HomePage implements OnInit {
 
   email: string = "";
+  listadoPosts: Post[] = [];
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private postService: PostService) {
 
     addIcons({ logOutOutline });
   }
@@ -27,5 +31,11 @@ export class HomePage implements OnInit {
     if (storedEmail) {
       this.email = storedEmail;
     }
+
+    setTimeout(()=> {
+      this.postService.getPosts().subscribe(posts => {
+        this.listadoPosts = posts;
+      })
+    }, 2000)
   }
 }
